882a386c76b50ecc7cd98a87abb94f8d9bd6104c
Wian van Aggelen <info@renosoft.nl>
2020-08-28 21:16:32 +0200

    Added ColumnLayout and RowLayout

--- src/modules/QtQuick.Layouts/ColumnLayout.js
+++ src/modules/QtQuick.Layouts/ColumnLayout.js
@@ -6,5 +6,28 @@ class QtQuick_Layouts_ColumnLayout extends QtQuick_Item {
     spacing: "real"
   };
 
-  // TODO
+  constructor(meta) {
+    super(meta);
+
+    this.childrenChanged.connect(this, this.$onChildrenChanged);
+
+    this.css.display = "flex";
+    this.css.flexDirection = "column";
+
+
+  }
+
+  $onChildrenChanged() {
+    this.children.forEach((child) => {
+      child.css.position = "";
+      child.css.top = "";
+      child.css.bottom = "";
+      child.css.right = "";
+      child.css.float = "";
+
+      child.css.display = "flex";
+      child.css.flex = "1 1 1";
+
+    })
+  }
 }
--- src/modules/QtQuick.Layouts/RowLayout.js
+++ src/modules/QtQuick.Layouts/RowLayout.js
@@ -6,5 +6,26 @@ class QtQuick_Layouts_RowLayout extends QtQuick_Item {
     spacing: "real"
   };
 
-  // TODO
+  constructor(meta) {
+    super(meta);
+
+    this.childrenChanged.connect(this, this.$onChildrenChanged);
+
+    this.css.display = "flex";
+    this.css.flexDirection = "row";
+  }
+
+  $onChildrenChanged() {
+    this.children.forEach((child) => {
+      child.css.position = "";
+      child.css.top = "";
+      child.css.bottom = "";
+      child.css.right = "";
+      child.css.float = "";
+
+      child.css.display = "flex";
+      child.css.flex = "1 1 1";
+
+    })
+  }
 }
